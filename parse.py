28da475bfea7ed9973ebe64616da01fe4bd0541c
estasney <16440579+estasney@users.noreply.github.com>
2019-04-09 14:25:35 -0400

    fix

--- parse.py
+++ parse.py
@@ -1,13 +1,16 @@
-import argparse
+import pandas as pd
 import binascii
 import io
 import json
 import math
 import os
+import glob
+import tempfile
 from datetime import datetime
 from struct import unpack
 
 from schema import PidTagSchema
+from pathlib import Path
 
 
 # this is the table of tags and codes
@@ -24,209 +27,217 @@ def lookup(ulPropID):
         return hex(ulPropID)
 
 
-parser = argparse.ArgumentParser()
-parser.add_argument('-o', '--output', help="Destination json file")
-parser.add_argument('oabfile', metavar='OABFILE', type=str, nargs=1,
-                    help=r'Path to OAB File. Typically found in '
-                         r'C:\Users\%username%\AppData\Local\Microsoft\Outlook\Offline Address Books\..\udetails.cab')
+class GalParser(object):
 
+    GAL_PATH = r"AppData\Local\Microsoft\Outlook\Offline Address Books\**\udetails.oab"
 
-def parse_oab(oab_path, output_path):
+    def __init__(self, output_path, gal_path=None):
+        self.output_path = output_path if output_path is not None else os.path.join(str(Path.home()), "output.csv")
+        self.gal_path = gal_path if gal_path is not None else self.get_gal_path()
+        self.temp_file_ = None
 
-    counter = 0
-    json_out = open(output_path, 'w')
+    @property
+    def temp_file(self):
+        if self.temp_file_ and os.path.exists(self.temp_file_):
+            return self.temp_file_
+        else:
+            self.temp_file_ = tempfile.NamedTemporaryFile("w+b")
+            return self.temp_file_
 
-    with open(oab_path, 'rb') as f:
-        (ulVersion, ulSerial, ulTotRecs) = unpack('<III', f.read(4 * 3))
-        assert ulVersion == 32, 'This only supports OAB Version 4 Details File'
+    def get_gal_path(self):
+        home_path = str(Path.home())
+        glob_path = os.path.join(home_path, self.GAL_PATH)
+        gal_path = glob.glob(glob_path, recursive=True)
+        if not gal_path:
+            raise FileNotFoundError
+        else:
+            return gal_path[0]
 
-        # OAB_META_DATA
-        cbSize = unpack('<I', f.read(4))[0]
+    def parse(self):
 
-        meta = io.BytesIO(f.read(cbSize - 4))
-        # the length of the header attributes
-        # we don't know and don't really need to know how to parse these
-        HDR_cAtts = unpack('<I', meta.read(4))[0]
+        json_out = self.temp_file
+        with open(self.gal_path, 'rb') as f:
+            (ulVersion, ulSerial, ulTotRecs) = unpack('<III', f.read(4 * 3))
+            assert ulVersion == 32, 'This only supports OAB Version 4 Details File'
 
-        for rgProp in range(HDR_cAtts):
-            ulPropID = unpack('<I', meta.read(4))[0]
-            ulFlags = unpack('<I', meta.read(4))[0]
+            # OAB_META_DATA
+            cbSize = unpack('<I', f.read(4))[0]
 
-        # these are the attributes that we actually care about
-        OAB_cAtts = unpack('<I', meta.read(4))[0]
-        OAB_Atts = []
+            meta = io.BytesIO(f.read(cbSize - 4))
+            # the length of the header attributes
+            # we don't know and don't really need to know how to parse these
+            HDR_cAtts = unpack('<I', meta.read(4))[0]
 
-        for rgProp in range(OAB_cAtts):
-            ulPropID = unpack('<I', meta.read(4))[0]
-            ulFlags = unpack('<I', meta.read(4))[0]
+            for rgProp in range(HDR_cAtts):
+                ulPropID = unpack('<I', meta.read(4))[0]
+                ulFlags = unpack('<I', meta.read(4))[0]
 
-            OAB_Atts.append(ulPropID)
+            # these are the attributes that we actually care about
+            OAB_cAtts = unpack('<I', meta.read(4))[0]
+            OAB_Atts = []
 
-        # OAB_V4_REC (Header Properties)
-        cbSize = unpack('<I', f.read(4))[0]
-        f.read(cbSize - 4)
+            for rgProp in range(OAB_cAtts):
+                ulPropID = unpack('<I', meta.read(4))[0]
+                ulFlags = unpack('<I', meta.read(4))[0]
 
-        # now for the actual stuff
-        while True:
-            read = f.read(4)
-            if read == b'':
-                break
-            # this is the size of the chunk, incidentally its inclusive
-            cbSize = unpack('<I', read)[0]
-            # so to read the rest, we subtract four
-            chunk = io.BytesIO(f.read(cbSize - 4))
-            # wow such bit op
-            presenceBitArray = bytearray(chunk.read(int(math.ceil(OAB_cAtts / 8.0))))
-            indices = [i for i in range(OAB_cAtts) if (presenceBitArray[i // 8] >> (7 - (i % 8))) & 1 == 1]
+                OAB_Atts.append(ulPropID)
 
-            def read_str():
-                # strings in the OAB format are null-terminated
-                buf = b""
-                while True:
-                    n = chunk.read(1)
-                    if n == b"\0" or n == b"":
-                        break
-                    buf += n
-                return buf.decode('utf-8')
+            # OAB_V4_REC (Header Properties)
+            cbSize = unpack('<I', f.read(4))[0]
+            f.read(cbSize - 4)
 
-            # return unicode(buf, errors="ignore")
+            # now for the actual stuff
+            while True:
+                read = f.read(4)
+                if read == b'':
+                    break
+                # this is the size of the chunk, incidentally its inclusive
+                cbSize = unpack('<I', read)[0]
+                # so to read the rest, we subtract four
+                chunk = io.BytesIO(f.read(cbSize - 4))
+                # wow such bit op
+                presenceBitArray = bytearray(chunk.read(int(math.ceil(OAB_cAtts / 8.0))))
+                indices = [i for i in range(OAB_cAtts) if (presenceBitArray[i // 8] >> (7 - (i % 8))) & 1 == 1]
 
-            def read_int():
-                # integers are cool aren't they
-                byte_count = unpack('<B', chunk.read(1))[0]
-                if 0x81 <= byte_count <= 0x84:
-                    byte_count = unpack('<I', (chunk.read(byte_count - 0x80) + b"\0\0\0")[0:4])[0]
-                else:
-                    assert byte_count <= 127, "byte count must be <= 127"
-                return byte_count
+                def read_str():
+                    # strings in the OAB format are null-terminated
+                    buf = b""
+                    while True:
+                        n = chunk.read(1)
+                        if n == b"\0" or n == b"":
+                            break
+                        buf += n
+                    return buf.decode('utf-8')
 
-            def stringify_bytes(record_data):
-                output_data = {}
-                for k, v in record_data.items():
-                    if isinstance(v, str):
-                        output_data[k] = v
-                    elif isinstance(v, bytes):
-                        output_data[k] = v.decode()
-                    elif isinstance(v, list):
-                        temp_list = []
-                        for val in v:
-                            if isinstance(val, bytes):
-                                try:
-                                    decoded = val.decode('utf-8')
-                                except:
-                                    decoded = ""
-                                temp_list.append(decoded)
-                            else:
-                                temp_list.append(val)
+                # return unicode(buf, errors="ignore")
 
-                        output_data[k] = temp_list
-                return output_data
+                def read_int():
+                    # integers are cool aren't they
+                    byte_count = unpack('<B', chunk.read(1))[0]
+                    if 0x81 <= byte_count <= 0x84:
+                        byte_count = unpack('<I', (chunk.read(byte_count - 0x80) + b"\0\0\0")[0:4])[0]
+                    else:
+                        assert byte_count <= 127, "byte count must be <= 127"
+                    return byte_count
 
+                def stringify_bytes(record_data):
+                    output_data = {}
+                    for k, v in record_data.items():
+                        if isinstance(v, str):
+                            output_data[k] = v
+                        elif isinstance(v, bytes):
+                            output_data[k] = v.decode()
+                        elif isinstance(v, list):
+                            temp_list = []
+                            for val in v:
+                                if isinstance(val, bytes):
+                                    try:
+                                        decoded = val.decode('utf-8')
+                                    except:
+                                        decoded = ""
+                                    temp_list.append(decoded)
+                                else:
+                                    temp_list.append(val)
 
+                            output_data[k] = temp_list
+                    return output_data
 
-            rec = {}
+                rec = {}
 
-            for i in indices:
-                PropID = hexify(OAB_Atts[i])
-                if PropID not in PidTagSchema:
-                    raise "This property id (" + PropID + ") does not exist in the schema"
+                for i in indices:
+                    PropID = hexify(OAB_Atts[i])
+                    if PropID not in PidTagSchema:
+                        raise "This property id (" + PropID + ") does not exist in the schema"
 
-                (Name, Type) = PidTagSchema[PropID]
+                    (Name, Type) = PidTagSchema[PropID]
 
-                if Type == "PtypString8" or Type == "PtypString":
-                    val = read_str()
-                    rec[Name] = val
-
-                elif Type == "PtypBoolean":
-                    val = unpack('<?', chunk.read(1))[0]
-                    rec[Name] = val
-
-                elif Type == "PtypInteger32":
-                    val = read_int()
-                    rec[Name] = val
-
-                elif Type == "PtypBinary":
-                    bin = chunk.read(read_int())
-                    rec[Name] = binascii.b2a_hex(bin)
-
-                elif Type == "PtypMultipleString" or Type == "PtypMultipleString8":
-                    byte_count = read_int()
-
-                    arr = []
-                    for i in range(byte_count):
+                    if Type == "PtypString8" or Type == "PtypString":
                         val = read_str()
-                        arr.append(val)
+                        rec[Name] = val
 
-                    rec[Name] = arr
+                    elif Type == "PtypBoolean":
+                        val = unpack('<?', chunk.read(1))[0]
+                        rec[Name] = val
 
-                elif Type == "PtypMultipleInteger32":
-                    byte_count = read_int()
-
-                    arr = []
-                    for i in range(byte_count):
+                    elif Type == "PtypInteger32":
                         val = read_int()
-                        if Name == "OfflineAddressBookTruncatedProperties":
-                            val = hexify(val)
-                            if val in PidTagSchema:
-                                val = PidTagSchema[val][0]
-                        arr.append(val)
+                        rec[Name] = val
 
-                    rec[Name] = arr
+                    elif Type == "PtypBinary":
+                        bin = chunk.read(read_int())
+                        rec[Name] = binascii.b2a_hex(bin)
 
-                elif Type == "PtypMultipleBinary":
-                    byte_count = read_int()
+                    elif Type == "PtypMultipleString" or Type == "PtypMultipleString8":
+                        byte_count = read_int()
 
-                    arr = []
-                    for i in range(byte_count):
-                        bin_len = read_int()
-                        bin = chunk.read(bin_len)
-                        arr.append(binascii.b2a_hex(bin))
+                        arr = []
+                        for i in range(byte_count):
+                            val = read_str()
+                            arr.append(val)
 
-                    rec[Name] = arr
-                else:
-                    raise "Unknown property type (" + Type + ")"
+                        rec[Name] = arr
 
-            remains = chunk.read()
-            if len(remains) > 0:
-                raise "This record contains unexpected data at the end: " + remains
+                    elif Type == "PtypMultipleInteger32":
+                        byte_count = read_int()
 
-            rec = stringify_bytes(rec)
+                        arr = []
+                        for i in range(byte_count):
+                            val = read_int()
+                            if Name == "OfflineAddressBookTruncatedProperties":
+                                val = hexify(val)
+                                if val in PidTagSchema:
+                                    val = PidTagSchema[val][0]
+                            arr.append(val)
 
-            json_out.write(json.dumps(rec) + '\n')
+                        rec[Name] = arr
+
+                    elif Type == "PtypMultipleBinary":
+                        byte_count = read_int()
+
+                        arr = []
+                        for i in range(byte_count):
+                            bin_len = read_int()
+                            bin = chunk.read(bin_len)
+                            arr.append(binascii.b2a_hex(bin))
+
+                        rec[Name] = arr
+                    else:
+                        raise "Unknown property type (" + Type + ")"
+
+                remains = chunk.read()
+                if len(remains) > 0:
+                    raise "This record contains unexpected data at the end: " + remains
+
+                rec = stringify_bytes(rec)
+
+                json_out.write(json.dumps(rec).encode() + b'\n')
+
+        # get the JSON format into something pandas can parse
+        json_out.seek(0)
+        raw_data = json_out.read()
+        encoded_data = raw_data.decode()
+        del raw_data
+        encoded_lines = encoded_data.splitlines()
+        del encoded_data
+        json_data = [json.loads(x) for x in encoded_lines]
+        del encoded_lines
+
+        df = pd.DataFrame(json_data)
+        del json_data
+        df.to_csv(self.output_path)
 
 
 if __name__ == "__main__":
+    output_path = input("Where should the JSON file be saved? Enter an empty string to save CSV file to user folder")
+    if output_path == "":
+        output_path = None
+    gal_path = input("Enter the path to GAL or a blank string to search for GAL")
+    if gal_path == "":
+        gal_path = None
+    gal_parse = GalParser(output_path=output_path, gal_path=gal_path)
     start_time = datetime.now()
-    args = parser.parse_args()
-    if args.output is None:
-        raise Exception("Output path is required")
-    else:
-        json_out = args.output
-
     # parse the oab file to newline separated json
     print("Starting .oab parse")
-    parse_oab(args.oabfile[0], json_out)
+    gal_parse.parse()
     elapsed = datetime.now() - start_time
     print("Finished .oab parse in {}".format(elapsed))
-
-
-    # read the output file into memory and write it back out as valid JSON
-
-    def get_original_json():
-        with open(args.output, 'r') as data_file:
-            return [json.loads(x) for x in data_file.read().splitlines()]
-
-
-    print("Validating JSON Data")
-    start = datetime.now()
-    data = get_original_json()
-    os.remove(args.output)
-    elapsed = datetime.now() - start
-    print("Finished Validating JSON in {}".format(elapsed))
-
-    print("Dumping JSON")
-    with open(args.output, 'w+') as json_file:
-        json.dump(data, json_file)
-    print("Finished JSON")
-    elapsed = datetime.now() - start_time
-    print("Finished process in {}".format(elapsed))
